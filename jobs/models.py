457af392f67b90da7f065a7ebf3976e5e1f8b9c3
Andrew Madrick <andrewmadrick@me.com>
2018-11-25 12:57:28 -0700

    Andy first commit

---
+++ jobs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    # Image
+    image = models.ImageField(upload_to='images/')
+    #Title
+    title = models.CharField(max_length=100, default='')
+    # Summary
+    summary = models.CharField(max_length=200)
+    # Song
+    song= models.CharField(max_length=200, default="Song: 'TITLE' - ARTIST (YEAR)")
+    # Art
+    art = models.CharField(max_length=200, default="Art: 'TITLE' - ARTIST (YEAR)")
+    # Write up
+    write_up = models.CharField(max_length=10000, default='')
+    # Spotify
+    spotify = models.CharField(max_length=200, default="Add 'embed' between '...com/' and '/track...'")
+    # Google Maps
+    #map = models.CharField(max_length=5000, default="Just add URL")
+
+    def __str__(self):
+        return self.summary
